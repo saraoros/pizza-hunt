61b5f5b26c7cb223afd7278691c29e7f6bbcfd5b
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-04-24 23:58:58 -0700

    added online capabilities

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 const commentRoutes = require("./comment-routes");
@@ -15,5 +17,9 @@ router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
 router.route("/:pizzaId/:commentId").delete(removeComment);
+
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
